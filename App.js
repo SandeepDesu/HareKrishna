43c934919098021dd96fb56472f9848b81c628f0
Sandy <dsandy.ip@gmail.com>
2017-12-16 02:54:39 -0800

    change design

--- App.js
+++ App.js
@@ -16,35 +16,32 @@ import {
   TouchableOpacity,
   Keyboard,
   Animated,
-  Easing
+  Easing,
+  Picker
 } from 'react-native';
-import { Item, Input,Button } from 'native-base';
-
+import { Item, Input, Button } from 'native-base';
+const one = require('./images/1.jpg');
+const two = require('./images/2.jpg');
+const three = require('./images/3.jpg');
+const four = require('./images/4.jpg');
+const five = require('./images/5.jpg');
+const imagePath = [
+  one, two, three, four, five
+]
+const randomNumber = Math.floor(Math.random() * 4);
+const backgroundColor = '#1a66ff'
 export default class App extends Component<{}> {
-
   constructor() {
     super();
     this.state = {
+      image: imagePath[randomNumber],
       totalRounds: 0,
       completedRounds: 0,
       count: 0,
       date: new Date().toLocaleDateString(),
       setRounds: 0,
-      started: true,
+      showSet: true
     };
-    this.animatedValue = new Animated.Value(0)
-  }
-
-  animate() {
-    this.animatedValue.setValue(0)
-    Animated.timing(
-      this.animatedValue,
-      {
-        toValue: 1,
-        duration: 5000,
-        easing: Easing.linear
-      }
-    ).start(() => this.animate())
   }
 
   componentDidMount() {
@@ -52,7 +49,7 @@ export default class App extends Component<{}> {
       if (value) {
         let a = JSON.parse(value);
         if (a && a.date === new Date().toLocaleDateString()) {
-          this.setState({ count: a.count, totalRounds: a.totalRounds, completedRounds: a.completedRounds, date: new Date().toLocaleDateString() });
+          this.setState({ count: a.count, totalRounds: a.totalRounds, completedRounds: a.completedRounds, date: new Date().toLocaleDateString(), showSet: a.totalRounds > 0 ? false : true });
         } else {
           this.setState({
             totalRounds: 0,
@@ -74,21 +71,17 @@ export default class App extends Component<{}> {
   setRounds = () => {
     let rounds = this.state.setRounds;
     if (rounds !== 0) {
-      this.setState({ totalRounds: rounds, setRounds: 0 });
-      this.refs.roundsInput._root.setNativeProps({ text: '' });
-      Keyboard.dismiss();
+      this.setState({ totalRounds: rounds, setRounds: 0, showSet: false });
     }
   }
 
   counting = () => {
     if (this.state.totalRounds !== 0 && this.state.totalRounds > this.state.completedRounds) {
-      if (this.state.started) {
-        this.animate();
-      }
+
       if (this.state.count < 107) {
-        this.setState({ count: this.state.count + 1, started: false });
+        this.setState({ count: this.state.count + 1 });
       } else {
-        this.setState({ count: 0, completedRounds: this.state.completedRounds + 1, started: false });
+        this.setState({ count: 0, completedRounds: this.state.completedRounds + 1 });
       }
 
       setTimeout(() => {
@@ -110,7 +103,8 @@ export default class App extends Component<{}> {
       completedRounds: 0,
       count: 0,
       date: new Date().toLocaleDateString(),
-      setRounds: 0
+      setRounds: 0,
+      showSet: true
     });
     AsyncStorage.setItem('count', JSON.stringify({
       totalRounds: 0,
@@ -121,64 +115,79 @@ export default class App extends Component<{}> {
     }));
   }
 
+
   render() {
-    const textSize = this.animatedValue.interpolate({
-      inputRange: [0, 0.5, 1],
-      outputRange: [20, 32, 20]
-    })
     return (
       <View style={styles.container}>
         <StatusBar
-          backgroundColor='#FFA500'
+          backgroundColor='#ff8c1a'
           barStyle="light-content"
         />
-        <Image
-          source={require('./images/1.png')} style={styles.imageContainer}>
-          <View style={styles.headerContainer}>
-            <Text style={styles.headerText}>Rounds : {this.state.completedRounds} / {this.state.totalRounds}</Text>
-            <Text style={styles.headerText}>Count : {this.state.count}</Text>
-            <Text style={styles.headerText}>{this.state.date}</Text>
-          </View>
-          <View style={styles.defaultContainer}>
-            <Text style={styles.quoteTextMain}>
-              Jai Sri Krishna Chaitanya Prabhu Nityananda Sri Advaita gadhadara srivasadhi goura baktha vrindha .....
-            </Text>
-          </View>
-          <View style={styles.defaultContainer}>
-            <Text style={styles.quoteTextMain}>
-              Hare Krishna hare Krishna Krishna Krishna hare hare Hare Rama hare Rama Rama Rama hare hare
-            </Text>
-          </View>
-          <View style={styles.container}>
-            <Animated.Text
-              style={{
-                fontSize: textSize,
-                marginTop: 10,
-                color: 'green',
-                fontWeight: 'bold'
-              }} >
-              Hare Krishna !!!!!!
-            </Animated.Text>
-          </View>
-          <View style={styles.setRoundsContainer}>
-            <View style={styles.defaultContainer}>
-              <Item rounded style={styles.inputBox}>
-                <Input ref='roundsInput' placeholder='Set Rounds' keyboardType={"numeric"} onChangeText={(v) => this.setState({ setRounds: v })} />
-              </Item>
+        <View style={styles.mainContainer}>
+          <View style={styles.subContainer}>
+            <View style={styles.roundsContainer}>
+              <Text style={styles.textStyle}>Rounds</Text>
+              <Text style={styles.textStyle}>{this.state.totalRounds}</Text>
             </View>
-            <TouchableOpacity style={styles.buttonContainer} onPress={() => this.setRounds()}>
-              <Text style={styles.buttonText}>Set</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.resetButtonContainer} onPress={() => this.reset()}>
-              <Text style={styles.buttonText}>Reset</Text>
-            </TouchableOpacity>
+            <View style={styles.roundsContainer}>
+              <Text style={styles.textStyle}>Count</Text>
+              <Text style={styles.textStyle}>{this.state.count}</Text>
+            </View>
+            <View style={styles.roundsContainer}>
+              <Text style={styles.textStyle}>Completed</Text>
+              <Text style={styles.textStyle}>{this.state.completedRounds}</Text>
+            </View>
           </View>
-          <View style={styles.footerContainer}>
-              <Button full success rounded onPress={() => this.counting()}>
-                <Text style={styles.buttonText}>Count</Text>
-              </Button>
+          <View style={styles.mainContainer}>
+            <View style={styles.quotesContainer}>
+              <Text style={styles.quotesText}> Jai Sri Krishna Chaitanya Prabhu Nityananda Sri Advaita gadhadara sri vasadhi goura baktha brundha ..... </Text>
+            </View>
+            <View style={styles.quotesContainer}>
+              <Text style={styles.quotesText}> Hare Krishna hare Krishna Krishna Krishna hare hare Hare Rama hare Rama Rama Rama hare hare </Text>
+            </View>
           </View>
-        </Image>
+        </View>
+        <Image
+          source={this.state.image} style={styles.imageContainer} />
+        <View style={styles.mainContainer}>
+          <View style={styles.setRoundsContainer}>
+            <View style={[styles.pickerBlock, { backgroundColor: this.state.showSet ? 'yellow' : backgroundColor }]}>
+              {this.state.showSet ? <Picker
+                selectedValue={this.state.setRounds}
+                onValueChange={(itemValue, itemIndex) => this.setState({ setRounds: itemValue })}>
+                <Picker.Item label="Select Rounds" value="0" />
+                <Picker.Item label="1" value="1" />
+                <Picker.Item label="2" value="2" />
+                <Picker.Item label="4" value="4" />
+                <Picker.Item label="8" value="8" />
+                <Picker.Item label="12" value="12" />
+                <Picker.Item label="16" value="16" />
+                <Picker.Item label="18" value="18" />
+                <Picker.Item label="20" value="20" />
+                <Picker.Item label="22" value="22" />
+                <Picker.Item label="25" value="25" />
+                <Picker.Item label="32" value="32" />
+                <Picker.Item label="64" value="64" />
+              </Picker> : null}
+            </View>
+            <View style={styles.picker}>
+              {this.state.showSet ? <TouchableOpacity style={styles.setButton} onPress={() => this.setRounds()}>
+                <Text style={styles.setButtonText}>Set</Text>
+              </TouchableOpacity> : null}
+            </View>
+            <View style={styles.picker}>
+              <TouchableOpacity style={styles.resetButton} onPress={() => this.reset()}>
+                <Text style={styles.resetButtonText}>Reset</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.mainContainer}>
+            <TouchableOpacity style={styles.countButton} onPress={() => this.counting()}>
+              <Text style={styles.countButtonText}>Count</Text>
+            </TouchableOpacity>
+            <Text style={styles.dateText}>{this.state.date}</Text>
+          </View>
+        </View>
       </View>
     );
   }
@@ -187,70 +196,97 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    backgroundColor: backgroundColor
   },
   imageContainer: {
     flex: 1,
     resizeMode: Image.resizeMode.contain,
-    justifyContent: 'space-between',
-    padding: 15
+    alignSelf: 'stretch',
+    width: undefined,
+    height: undefined
   },
-  headerContainer: {
+  mainContainer: {
+    flex: 1,
+    margin: 2
+  },
+  subContainer: {
     flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-around'
   },
-  headerText: {
-    fontSize: 22,
-    color: '#FFA500'
+  roundsContainer: {
+    flex: 1,
+    margin: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  textStyle: {
+    fontSize: 18,
+    color: 'white'
+  },
+  quotesContainer: {
+    flex: 1,
+    margin: 1
+  },
+  quotesText: {
+    fontSize: 14,
+    textAlign: 'center',
+    color: 'white'
   },
   setRoundsContainer: {
     flex: 1,
-    justifyContent: 'space-between',
     flexDirection: 'row',
-    margin:5
+    margin: 5,
+    justifyContent: 'space-around',
   },
-  defaultContainer: {
+  picker: {
     flex: 1,
+    margin: 1
   },
-  quoteTextMain: {
-    fontSize: 16,
-    color: '#0040ff'
-  },
-  inputBox: {
-    borderColor: '#FFA500',
-    borderWidth: 3
-  },
-  footerContainer: {
-    justifyContent: "flex-end",
-  },
-  subFooter: {
-    flexDirection: 'row'
-  },
-  buttonContainer: {
-    width: 100,
+  pickerBlock: {
+    flex: 1,
     height: 50,
-    borderWidth: 2,
-    borderColor: '#FFA500',
-    borderRadius: 25,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#FFA500'
+    borderRadius: 25
   },
-  resetButtonContainer: {
-    width: 100,
+  setButton: {
+    width: undefined,
     height: 50,
-    borderWidth: 2,
-    borderColor: 'red',
-    borderRadius: 25,
-    alignItems: 'center',
+    backgroundColor: 'green',
     justifyContent: 'center',
-    backgroundColor: 'red'
+    alignItems: 'center',
+    borderRadius: 25
   },
-  buttonText: {
+  setButtonText: {
     fontSize: 16,
-    fontWeight: 'bold',
     color: 'white'
+  },
+  resetButton: {
+    width: undefined,
+    height: 50,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 25
+  },
+  resetButtonText: {
+    fontSize: 16,
+    color: 'white'
+  },
+  countButton: {
+    width: undefined,
+    height: 50,
+    backgroundColor: '#ff8c1a',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 25
+  },
+  countButtonText: {
+    fontSize: 16,
+    color: 'white'
+  },
+  dateText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf:'center'
   }
-
 });
